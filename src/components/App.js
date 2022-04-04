334ab036a7255847b5efb3ccbd17b45fc178c294
Gregory Olive <gregoryolive@outlook.com>
2022-04-04 13:41:20 +0900

    Add header and remove README content from create-react-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import '../styles/App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
+
+      <Routes>
+
+      </Routes>
+    </Router>
   );
 }
 
